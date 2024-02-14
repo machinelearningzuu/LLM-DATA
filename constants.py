847b8a2632b0849dd5d705a4ba7a80998440210f
Isuru Alagiyawanna <41842488+machinelearningzuu@users.noreply.github.com>
2024-02-14 20:10:20 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 gpt_flag = 'GPT4'
+llm_prefix = 'KAYA'
 embedding_flag = 'OPENAI'
 # working_dir = "/Users/1zuu/Library/Mobile Documents/com~apple~CloudDocs/ML DEV/KayaTech/Taysha Finetuning"
 working_dir = "/home/external/TayshaFinetuning/LLM-DATA"
