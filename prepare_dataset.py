0b58bd09683853e1b099f98c1b8adf412e5da502
Isuru Alagiyawanna <41842488+machinelearningzuu@users.noreply.github.com>
2024-02-15 13:59:10 +0530

    Update prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,20 +1,26 @@
 import json, os
 import pandas as pd
 
-def load_qna_pairs(data_dir = "generated/biotech"):
+def load_qna_pairs(data_dirs = [
+                                "generated/biotech",
+                                "generated/biotech-lfs"
+                                ]):
     qa_pair_array = []
-    for file in os.listdir(data_dir):
-        with open(os.path.join(data_dir, file), "r") as f:
-            try:
-                qa_pairs = json.load(f)
-                qa_pair_array.extend(qa_pairs)
-                    
-            except:
-                print(f"Error reading file {file}")
+    total_nodes = 0
+    for data_dir in data_dirs:
+        for file in os.listdir(data_dir):
+            with open(os.path.join(data_dir, file), "r") as f:
+                try:
+                    qa_pairs = json.load(f)
+                    qa_pair_array.extend(qa_pairs)
+                    total_nodes += 1
+                
+                except:
+                    print(f"Error reading file {file}")
 
-    return qa_pair_array
+    return qa_pair_array, total_nodes
 
-qa_pair_array = load_qna_pairs()
+qa_pair_array, total_nodes = load_qna_pairs()
 df_qa = pd.DataFrame(qa_pair_array)
 df_qa.drop_duplicates(
                     subset=['answer', 'context'],  
@@ -26,6 +32,6 @@ df_qa = df_qa[~df_qa.question.str.contains("Question 2:")]
 df_qa = df_qa[df_qa.question != ""]
 
 print("=================================")
-print("Total number of nodes : {}".format(len(os.listdir("generated/biotech"))))
+print("Total number of nodes : {}".format(total_nodes))
 print("Total number of QA pairs : {}".format(len(df_qa)))
 print("=================================")
