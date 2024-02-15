d64ca7dc809a5cc45fcefb6baaef5ccda984f33e
Isuru Alagiyawanna <41842488+machinelearningzuu@users.noreply.github.com>
2024-02-15 07:46:24 +0530

    Add files via upload

---
+++ valudate-dataset.py
@@ -0,0 +1,9 @@
+import json, os
+
+data_dir = "generated/biotech"
+for file in os.listdir(data_dir):
+    with open(os.path.join(data_dir, file), "r") as f:
+        qa_pairs = json.load(f)
+        # check if the file is empty
+        if len(qa_pairs) == 0:
+            print(f"file {file} is empty")
